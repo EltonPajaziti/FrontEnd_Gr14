f5ac742988da8628720fbc23ea080ba4c7a550ae
ZanaShabani <zana.shabani2@student.uni-pr.edu>
2025-05-06 17:54:22 +0200

    KAN-38 Initial version of Home page

---
+++ sms/src/Components/Footer.jsx
@@ -0,0 +1,38 @@
+import './Footer.css';
+
+function Footer(){
+    return(<footer className="footeri pt-5 pb-4">
+        <div className="sms1">
+          <h1>SMS</h1>
+          <p>University of Prishtina</p>
+        </div>
+        <hr />
+        <div className="contact">
+          <div className="c1">
+            <h3>Contact</h3>
+            <p>Adress: Rr. "George Bush", p.n., 10 000 Prishtinë, Republic of Kosovo</p>
+            <p>Tel: +383 38 452 122/189 973</p>
+            <br />
+            <p>E-Mail: sms@gmail.com</p>
+          </div>
+        
+        <div className="social">
+          <div class="social1">
+        <a href="https://facebook.com"><i className="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
+        </div>
+        <div class="social2">
+        <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
+        <a href="https://pinterest.com"><i className="fab fa-pinterest-p"></i></a>
+        </div>
+      </div>
+      </div>
+      
+        <div className="copyr">
+          <div className="footer-bottom">
+            <p>&copy; 2025 SMS</p>
+          </div>
+        </div>
+      </footer>)
+}
+export default Footer;
---
+++ sms/src/Pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import sms2 from '../assets/sms2.jpg';
+import uni1 from '../assets/uni1.jpg';
+import unii2 from '../assets/unii2.png';
+import unii3 from '../assets/unii3.png';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+    <Navbar />
+    <div class="home">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+            <div class="carousel-item active c-item">
+                <img src={unii2} class="d-block w-100 c-img"/>
+            </div>
+            <div class="carousel-item c-item">
+                <img src={unii3} class="d-block w-100 c-img"/>
+            </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    <div className="UP-text-container">
+        <div className="Up-text-header">
+        <h1>The University of Prishtina is the academic heart of Kosovo, making an impact beyond borders</h1>
+        </div>
+        <div className="Up-text-paragraph">
+        <p>The University of Prishtina is located in the capital of Kosovo and stands as the oldest and largest higher education institution in the country. With thousands of dedicated students and academic staff, it forms a dynamic scientific and educational community. In the region, the University is recognized for its ongoing contribution to society, education, and scientific research. Since its establishment on February 13, 1970, the University of Prishtina has played a vital role in shaping a brighter future for new generations.</p>
+        </div>
+    </div>
+    <Footer />
+
+        </>
+
+    )
+}
+
+export default Home;
