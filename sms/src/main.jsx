e9dc2fb39d3b7ca951cd49275cb6ae2cb7d62181
ZanaShabani <zana.shabani2@student.uni-pr.edu>
2025-05-06 18:11:39 +0200

    KAN-38 Configuration of Bootstrap

--- sms/src/main.jsx
+++ sms/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Test from './Pages/Test';
 import Login from './Pages/Login';
+import Home from './Pages/Home';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-    <Login />
+    <Home />
     </>
   </React.StrictMode>
 );
